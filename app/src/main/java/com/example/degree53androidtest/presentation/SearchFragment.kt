ba7937131bdfba08e43c9b741d48b7bca92c29e2
Jose Garcia <jose.gbel@gmail.com>
2020-05-22 09:07:14 +0100

    view model recycler view added in the fragment

--- app/src/main/java/com/example/degree53androidtest/presentation/SearchFragment.kt
+++ app/src/main/java/com/example/degree53androidtest/presentation/SearchFragment.kt
@@ -5,12 +5,34 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
+import androidx.lifecycle.ViewModelProvider
+import androidx.recyclerview.widget.RecyclerView
 import com.example.degree53androidtest.R
+import com.example.degree53androidtest.business.viewmodels.SearchViewModel
+import com.example.degree53androidtest.presentation.adapters.ReposRecyclerViewAdapter
 
 class SearchFragment : Fragment() {
-    override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?,
-                              savedInstanceState: Bundle?): View {
+
+    private var reposAdapter: ReposRecyclerViewAdapter? = null
+    private lateinit var recyclerView : RecyclerView
+    private lateinit var viewModel: SearchViewModel
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View {
 
         return inflater.inflate(R.layout.search_fragment, container, false)
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        recyclerView = view.findViewById(R.id.search_recycler_view)
+    }
+
+    override fun onActivityCreated(savedInstanceState: Bundle?) {
+        super.onActivityCreated(savedInstanceState)
+        viewModel = ViewModelProvider(this).get(SearchViewModel::class.java)
+        
     }
 }
---
+++ app/src/main/java/com/example/degree53androidtest/presentation/adapters/ReposRecyclerViewAdapter.kt
@@ -0,0 +1,5 @@
+package com.example.degree53androidtest.presentation.adapters
+
+class ReposRecyclerViewAdapter {
+
+}
